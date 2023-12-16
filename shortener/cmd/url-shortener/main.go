0c69103d9056e9df9aa61da12e5d9ccd7c4e4a87
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-12-16 19:26:43 +0300

    Initial commit

---
+++ shortener/cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// ToDo: init config: cleanenv,
+
+	// ToDo: init logger: slog
+
+	// ToDo: init storage: sqlite
+
+	// ToDo: init router: chi, render
+
+	// ToDo: init server:
+}
